0734359e11d4a8beaab3c32d04b2c95d07acd65b
Jitchanok Ch <jtchnk@Jitchanoks-MacBook-Pro.local>
2022-02-23 15:04:49 +0700

    setup dbd project

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const minArr = [];
-  let min = 1;
-  for (min = 1; min <= 60; min++) {
-    minArr.push(min);
-  }
-
   return (
     <div className='bg-green-300'>
       <h1 className='text-3xl font-bold'>
